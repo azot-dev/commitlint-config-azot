7280af04713ee68de4f4dbc064932ddf38071c7f
Xavier Le Cunff <lecunffxavier@gmail.com>
2021-05-24 09:27:41 +0200

    feat: change error description

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
       'always',
       parsed => {
         if (!parsed.header.match(/^\[:.+:.+]/)) {
-          return [false, `🤪 Your commit has to be done with cz-emoji-azot for this project\nnpm i -g cz-emoji-azot`]
+          return [false, `🤪 Your commit has to be done with cz-emoji-azot\nℹ️ use "git cz" instead of "git commit -m something"\n`]
         }
         if (!parsed.header.match(/^\[:.+:.+].+/)) {
           return [false, `🤭 You must provide at least a short description`]
